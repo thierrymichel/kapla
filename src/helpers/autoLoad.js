4545db32b1ba65fa4bcc694c23ae242b9a4e035b
Thierry Michel <thmichel@gmail.com>
2018-09-26 10:28:13 +0200

    :boom: add "stripFolders" option for "autoLoad" because internal folders architecture has changed

--- src/helpers/autoLoad.js
+++ src/helpers/autoLoad.js
@@ -3,11 +3,14 @@
  *
  * @export
  * @param {any} context webpack context
+ * @param {object} options options
  * @returns {Array} definitions
  */
-export function autoLoad(context) {
+export function autoLoad(context, options = {
+  stripFolders: 1,
+}) {
   return context.keys()
-    .map(key => getDefinition(context, key))
+    .map(key => getDefinition(context, key, options))
     .filter(value => value);
 }
 
@@ -16,10 +19,11 @@ export function autoLoad(context) {
  *
  * @param {any} context webpack context
  * @param {String} key component filename/path
+ * @param {object} options options
  * @returns {Object} definition
  */
-function getDefinition(context, key) {
-  const slug = getSlug(key);
+function getDefinition(context, key, options) {
+  const slug = getSlug(key, options.stripFolders);
 
   if (slug) {
     return buildDefinition(context(key), slug);
@@ -52,14 +56,19 @@ function buildDefinition(module, slug) {
  * Get slug from filename/path
  *
  * @param {String} key component filename/path
+ * @param {Number} stripFolders strip folders from path (starts from "base")
  * @returns {String} component name
  */
-function getSlug(key) {
+function getSlug(key, stripFolders) {
   // [folder/[subfolders/]]MyComponentFileName.js
   const regex = /^(?:\.\/)?([A-Z]{1}[A-Za-z]+|[a-z/]+\/[A-Z]{1}[A-Za-z]+)(?:\.js?)$/;
-  const [, logicalName] = key.match(regex) || [];
+  let [, logicalName] = key.match(regex) || [];
 
   if (logicalName) {
+    for (let i = 0; i < stripFolders; i++) {
+      logicalName = logicalName.replace(/^([a-zA-Z]+\/)/, '');
+    }
+
     return logicalName // Ex: 1. AbbAbb || 2. bb/bb/AbbAbb
       .replace(/([A-Z])/g, '-$1') // 1. -Abb-Abb || 2. bb/bb/-Abb-Abb
       .replace(/^-/, '') // 1. Abb-Abb
